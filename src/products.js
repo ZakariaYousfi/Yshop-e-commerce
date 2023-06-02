933b21e9bfa0f9a4d0fb5f80b76887faa1de986a
Yousfi Zakaria <zakonaser@gmail.com>
2023-06-02 12:04:52 +0100

    half d

--- src/products.js
+++ src/products.js
@@ -9,7 +9,7 @@ const products = [
       price: 100,
       images:[{
         id:0,
-        url:'https://upload.wikimedia.org/wikipedia/commons/f/f6/Icon_Einstein_256x256.png?20130512134906',
+        url:'https://cdn.shopify.com/s/files/1/2538/1942/products/Black-Blank3_1_600x600_crop_center.png?v=1665434810',
       }],
       submitterAvatarUrl: 'images/avatars/daniel.jpg',
       productImageUrl: 'images/products/image-aqua.png',
@@ -21,7 +21,7 @@ const products = [
       url: '#',      
       images:[{
         id:1,
-        url:'https://upload.wikimedia.org/wikipedia/commons/f/f6/Icon_Einstein_256x256.png?20130512134906',
+        url:'https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22ss/img/products/contentsArea_itemimg_16.jpg',
       }],
       price: 80,
       submitterAvatarUrl: 'images/avatars/kristy.png',
@@ -34,7 +34,7 @@ const products = [
       url: '#',      
       images:[{
         id:2,
-        url:'https://upload.wikimedia.org/wikipedia/commons/f/f6/Icon_Einstein_256x256.png?20130512134906',
+        url:'https://assets.hermes.com/is/image/hermesproduct/hermes-hippomobile-mug--048131P-worn-1-0-0-1000-1000_g.jpg',
       }],
       price: 5,
       submitterAvatarUrl: 'images/avatars/veronika.jpg',
@@ -47,7 +47,7 @@ const products = [
       url: '#',
       images:[{
         id:3,
-        url:'https://upload.wikimedia.org/wikipedia/commons/f/f6/Icon_Einstein_256x256.png?20130512134906',
+        url:'https://www.mensjournal.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk2OTY3NjI5NjkxMjk5NDY4/air-jordan-1-mid-basketball-shoes-2.jpg',
       }],
       price: 400,
       submitterAvatarUrl: 'images/avatars/molly.png',
